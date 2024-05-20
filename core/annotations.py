Implemented rotation for imager and boundaries. Has a bug currently - will be fixed in next commit

core/annotations.py
@@ -1,4 +1,3 @@
-import inspect
 from typing import List, Optional, Union
 
 import numpy as np
@@ -74,7 +73,7 @@ class Border:
             y_max (Optional - int): new max y value of border
 
         """
-        assert x_max > x_min > 0 and y_min > y_max > 0, "Invalid image border. Border is 0 or negative."
+        assert x_max > x_min >= 0 and y_max > y_min >= 0, "Invalid image border. Border is 0 or negative."
         if x_min is not None:
             self.__x_min = x_min
         if x_max is not None:
@@ -124,7 +123,7 @@ class Annotation:
             boundary_type (str): type of boundary - accepted types are: BBox, KeyP, Poly or an empty string
         """
         self.__boundary: Boundary | None = None
-        self.set_boundary(boundary_points, boundary_type)
+        self.set_boundary(boundary_points, boundary_type, (image_height, image_width))
         self.__label: Label | None = None
         self.set_label(label_id, label_name)
         self.__border: Border | None = None
@@ -163,17 +162,14 @@ class Annotation:
         if label_name is not None:
             self.__label.name = label_name
 
-    def set_boundary(self, points: np.ndarray, boundary_type: Optional[str] = None) -> None:
+    def set_boundary(self, points: np.ndarray, boundary_type: str, img_dims: tuple) -> None:
         if self.__boundary is None:
-            if boundary_type is None:
-                raise ValueError("Expected boundary type when initializing new boundary. Found None.")
+            for obj in BOUNDARY_TYPE_OBJS:
+                if obj.__name__ == boundary_type + BOUNDARY_NAME:
+                    self.__boundary = obj(points, img_dims)
+                    break
             else:
-                for obj in BOUNDARY_TYPE_OBJS:
-                    if obj.__name__ == boundary_type + BOUNDARY_NAME:
-                        self.__boundary = obj(points)
-                        break
-                else:
-                    raise ValueError(f"Boundary type '{boundary_type}' could not be found.")
+                raise ValueError(f"Boundary type '{boundary_type}' could not be found.")
         else:
             self.__boundary.set(points)
 
@@ -215,6 +211,12 @@ class Annotation:
         """
         return self.__boundary.area
 
+    def clip(self):
+        """
+        Clips boundary to border
+        """
+        # self.__boundary.clip(0, self.__border.width, 0, self.__border.height)
+
 
 class Annotations:
     """
@@ -224,7 +226,6 @@ class Annotations:
     -> add all annots of all Annotations to first annotation
     """
     def __init__(self, image_width: int, image_height: int, boundary_type: str) -> None:
-
         self.annots: List[Annotation] = []
         self.width = image_width
         self.height = image_height
@@ -267,7 +268,7 @@ class Annotations:
         if y_max is not None:
             self.height -= self.height - y_max
 
-    def add(self, label_id: int, boundary_points: np.ndarray,label_name: Optional[str] = None) -> None:
+    def add(self, label_id: int, boundary_points: np.ndarray, label_name: Optional[str] = None) -> None:
         """
         Adds a new annotation.
         Args:
@@ -295,7 +296,7 @@ class Annotations:
                 annotation.valid = False
         self.clean()
     
-    def shift(self, x_shift: Optional[float] = None, y_shift: Optional[float] = None):
+    def shift(self, x_shift: Optional[float] = 0, y_shift: Optional[float] = 0):
         """
         Shifts boundaries of all annotations. If shift is None - no shift on that axis is performed.
         Args:
@@ -304,8 +305,9 @@ class Annotations:
         """
         for annot in self.annots:
             annot.boundary.shift(x_shift, y_shift)
+            annot.clip()
 
-    def scale(self, x_scale: Optional[float] = None, y_scale: Optional[float] = None):
+    def scale(self, x_scale: Optional[float] = 1, y_scale: Optional[float] = 1):
         """
         Scales borders and boundaries of all annotations. If scale is None - no scale on that axis is performed.
         Args:
@@ -320,6 +322,7 @@ class Annotations:
                 0,
                 int(y_scale * annot.border.height)
             )
+            annot.clip()
 
     def rotate(self, angle: float):
         """
@@ -329,4 +332,5 @@ class Annotations:
         """
         for annot in self.annots:
             annot.boundary.rotate(angle)
+            annot.clip()
 

core/augmentations.py
@@ -15,12 +15,10 @@ from ..utils.mat_utils import get_2d_transf_mat
 class Shift(SITransform):
     def __init__(
             self,
-            image: Optional[np.ndarray] = None,
-            annotations: Optional[Annotations] = None,
             x_shift: Optional[float] = None,
             y_shift: Optional[float] = None
     ) -> None:
-        super().__init__(image, annotations)
+        super().__init__()
         self.x_shift = x_shift
         self.y_shift = y_shift
 
@@ -31,39 +29,70 @@ class Shift(SITransform):
         self.annots.shift(self.x_shift, self.y_shift)
 
 
+class Scale(SITransform):
+    def __init__(
+            self,
+            x_scale: Optional[float] = None,
+            y_scale: Optional[float] = None
+    ) -> None:
+        super().__init__()
+        self.x_scale = x_scale if x_scale is not None else 1
+        self.y_scale = y_scale if y_scale is not None else 1
+
+    def _apply_on_image(self):
+        self.image = sp.ndimage.zoom(self.image, (self.x_scale, self.y_scale, 1))
+
+    def _apply_on_annots(self):
+        self.annots.scale(self.x_scale, self.y_scale)
 
 
-def scale():
-    pass
+class Rotate(SITransform):
+    def __init__(
+            self,
+            angle: float
+    ) -> None:
+        super().__init__()
+        self.angle = angle
+
+    def _apply_on_image(self):
+        self.image = sp.ndimage.rotate(self.image, self.angle, reshape=False)
+
+    def _apply_on_annots(self):
+        self.annots.rotate(self.angle)
 
 
-def rotate():
-    pass
 
-
-def transform():
-    pass
-
-
-def mosaic():
-    pass
-
-
-def overlay():
-    pass
-
-
-def crop():
-    pass
-
-
-def resize():
-    pass
-
-
-def cutout():
-    pass
-
-
-def mixup():
-    pass
+# def scale():
+#     pass
+#
+#
+# def rotate():
+#     pass
+#
+#
+# def transform():
+#     pass
+#
+#
+# def mosaic():
+#     pass
+#
+#
+# def overlay():
+#     pass
+#
+#
+# def crop():
+#     pass
+#
+#
+# def resize():
+#     pass
+#
+#
+# def cutout():
+#     pass
+#
+#
+# def mixup():
+#     pass

core/boundaries.py
@@ -1,12 +1,14 @@
+import math
 from typing import List, Optional, Union
 
 import numpy as np
 
 
 class Boundary:
-    def __init__(self, points: np.ndarray):
-        self.__points = points
-        self.validate()
+    def __init__(self, points: np.ndarray, img_dims: tuple):
+        self._points = points
+        self.img_dims = img_dims
+        # self.validate()
         self.valid = True
 
     def validate(self):
@@ -25,7 +27,7 @@ class Boundary:
         Raises:
             AssertionError if shape of boundary points does not match with (n, 2)
         """
-        boundary_shape = np.shape(self.__points)
+        boundary_shape = np.shape(self._points)
         assert len(boundary_shape) == boundary_shape[1] == 2, (f"Expected boundary of shape (n, 2). "
                                                                f"Received shape {boundary_shape}.")
 
@@ -36,17 +38,18 @@ class Boundary:
 
         Validates points of boundary depending on the boundary type.
         """
+        pass
 
     def _boundary_in_border(self):
         """
         Invalidates boundary if all values are equal.
         """
-        points_x, points_y = self.__points.T
+        points_x, points_y = self._points.T
         self.valid = len(np.unique(points_x)) != 1 and len(np.unique(points_y)) != 1
 
     @property
     def points(self) -> np.ndarray:
-        return self.__points
+        return self._points
 
     @property
     def center(self):
@@ -56,11 +59,15 @@ class Boundary:
     def area(self):
         return self.__get_area()
 
+    @property
+    def visualize(self):
+        return self._points
+
     def __get_center(self):
         """
         Calculates the center of the boundary by the mid-point between min and max x and y.
         """
-        points_x, points_y = self.__points.T
+        points_x, points_y = self._points.T
         return np.array(
             [
                 (np.min(points_x) + np.max(points_x)) / 2,
@@ -77,7 +84,7 @@ class Boundary:
         """
 
     def set(self, points: np.ndarray, validate: bool = True):
-        self.__points = points
+        self._points = points
         if validate:
             self.validate()
 
@@ -97,7 +104,7 @@ class Boundary:
             y_min (Optional - int): new min y value of boundary points
             y_max (Optional - int): new max y value of boundary points
         """
-        points_x, points_y = self.__points.T
+        points_x, points_y = self._points.T
         self.set(
             np.vstack(
                 (
@@ -114,7 +121,7 @@ class Boundary:
             x_shift (float): Shift of x coordinates
             y_shift (float): Shift of y coordinates
         """
-        self.set(self.__points + np.array([x_shift, y_shift]))
+        self.set(self._points + np.array([x_shift, y_shift]))
 
     def scale(self, x_scale: float, y_scale: float):
         """
@@ -129,7 +136,7 @@ class Boundary:
                 [0, y_scale]
             ]
         )
-        self.set(np.einsum("bi, ij -> bi", self.__points, matrix))
+        self.set(np.einsum("bi, ij -> bi", self._points, matrix))
 
     def rotate(self, angle: float):
         """
@@ -137,35 +144,78 @@ class Boundary:
         Args:
             angle (float): Angle of rotation in deg
         """
-        rad_angle = np.deg2rad(angle)
+        rad_angle = np.deg2rad(-angle)
         matrix = np.array(
             [
                 [np.cos(rad_angle), -np.sin(rad_angle)],
                 [np.sin(rad_angle), np.cos(rad_angle)]
             ]
         )
-        self.set(np.einsum("bi, ij -> bi", self.__points, matrix))
+        # ox, oy = self.center
+        # adj_points_list = []
+        # for point in self._points:
+        #     x, y = point
+        #     qx = ox + math.cos(rad_angle) * (x - ox) + math.sin(rad_angle) * (y - oy)
+        #     qy = oy + -math.sin(rad_angle) * (x - ox) + math.cos(rad_angle) * (y - oy)
+        #     adj_points_list.append([qx, qy])
+        # points = np.array(adj_points_list)
+
+        adj_points = self._points - self.center
+        points = np.einsum("bi, ij -> bj", adj_points, matrix) + self.center
+        #points = np.vstack([np.matmul(point, matrix) for point in adj_points]) + self.center
+
+        # points = np.einsum("bi, ij -> bi", self.__points, matrix)
+        self.set(points)
 
 
 class BBoxBoundary(Boundary):
     """
     Boundary for bounding boxes
     """
-    def __init__(self, points: np.ndarray):
-        super().__init__(points)
+    def __init__(self, points: np.ndarray, img_dims: tuple):
+        self.__min_max_points = None
+        super().__init__(points, img_dims)
+
+    @property
+    def points(self) -> np.ndarray:
+        return self.__min_max_points
+
+    @property
+    def visualize(self):
+        points_x, points_y = self.points.T
+        return np.array(
+            [
+                [min(points_x), min(points_y)],
+                [min(points_x), max(points_y)],
+                [max(points_x), max(points_y)],
+                [max(points_x), min(points_y)]
+            ]
+        )
 
     def _validate_points(self):
         """
         Forces boundary as min-max box.
         """
-        points_x, points_y = self.points.T
+        points_x, points_y = self._points.T
+        min_x = min(points_x)
+        min_y = min(points_y)
+        max_x = max(points_x)
+        max_y = max(points_y)
         self.set(
             np.array(
                 [
-                    [min(points_x), min(points_y)],
-                    [max(points_x), max(points_y)]
+                    [min_x, min_y],
+                    [max_x, min_y],
+                    [max_x, max_y],
+                    [min_x, max_y]
                 ]
             ), validate=False
+        )
+        self.__min_max_points = np.array(
+                [
+                    [min_x, min_y],
+                    [max_x, max_y]
+                ]
         )
 
     def __get_area(self) -> float:
@@ -179,8 +229,8 @@ class KeyPBoundary(Boundary):
     """
     Boundary for keypoints.
     """
-    def __init__(self, points: np.ndarray):
-        super().__init__(points)
+    def __init__(self, points: np.ndarray, img_dims: tuple):
+        super().__init__(points, img_dims)
 
     def __get_area(self) -> float:
         """
@@ -193,5 +243,5 @@ class PolyBoundary(Boundary):
     """
     Boundary for polygons.
     """
-    def __init__(self, points: np.ndarray):
-        super().__init__(points)
+    def __init__(self, points: np.ndarray, img_dims: tuple):
+        super().__init__(points, img_dims)