Fixed bug with rotation, Implemented Resize and Mosaic Augmentation

core/annotations.py
@@ -5,6 +5,7 @@ import numpy as np
 from ..utils import new_id
 
 from .boundaries import Boundary, BBoxBoundary, KeyPBoundary, PolyBoundary
+from.borders import ImageBorder
 
 BOUNDARY_NAME = "Boundary"
 BOUNDARY_TYPE_OBJS = [BBoxBoundary, KeyPBoundary, PolyBoundary]
@@ -22,93 +23,12 @@ class Label:
         self.name = name
 
 
-class Border:
-    def __init__(self, width: int, height: int) -> None:
-        """
-        Defines a border of an image.
-        Args:
-            width (int): width of image in pixels
-            height (int): height of image in pixels
-        """
-        self.__width = width
-        self.__height = height
-
-        self.__x_min = 0
-        self.__x_max = self.__width
-        self.__y_min = 0
-        self.__y_max = self.__height
-
-    @property
-    def width(self):
-        return self.__width
-
-    @property
-    def height(self):
-        return self.__height
-
-    @property
-    def corners(self):
-        return np.array(
-            [
-                [self.__x_min, self.__x_max],
-                [self.__y_min, self.__y_max]
-            ], np.int32
-        )
-
-    def set(
-            self,
-            x_min: Optional[int] = None,
-            x_max: Optional[int] = None,
-            y_min: Optional[int] = None,
-            y_max: Optional[int] = None
-    ) -> None:
-        """
-        Sets corner points of border. This adapts the border to crops or scaling of the image. Input values cannot be
-        negative. Corner points are used for further calculation until the border gets rebased. Values which are not
-        provided will stay as is.
-        Args:
-            x_min (Optional - int): new min x value of border
-            x_max (Optional - int): new max x value of border
-            y_min (Optional - int): new min y value of border
-            y_max (Optional - int): new max y value of border
-
-        """
-        assert x_max > x_min >= 0 and y_max > y_min >= 0, "Invalid image border. Border is 0 or negative."
-        if x_min is not None:
-            self.__x_min = x_min
-        if x_max is not None:
-            self.__x_max = x_max
-        if y_min is not None:
-            self.__y_min = y_min
-        if y_max is not None:
-            self.__y_max = y_max
-
-    def reset(self) -> None:
-        """
-        Resets the current border Corner points.
-        """
-        self.__x_min = 0
-        self.__x_max = self.__width
-        self.__y_min = 0
-        self.__y_max = self.__height
-
-    def rebase(self) -> None:
-        """
-        Rebase the border by calculating the new width and height from the current corner points. Resets the border
-        subsequently.
-        """
-        self.__width = self.__x_max - self.__x_min
-        self.__height = self.__y_max - self.__y_min
-        self.reset()
-
-
 class Annotation:
     def __init__(
             self,
             label_id: int,
             boundary_points: np.ndarray,
-            image_width: int,
-            image_height: int,
+            image_border: ImageBorder,
             boundary_type: str,
             label_name: Optional[str] = None
     ) -> None:
@@ -122,12 +42,11 @@ class Annotation:
             image_height (int): image height in pixel
             boundary_type (str): type of boundary - accepted types are: BBox, KeyP, Poly or an empty string
         """
+        self.__border: ImageBorder = image_border
         self.__boundary: Boundary | None = None
-        self.set_boundary(boundary_points, boundary_type, (image_height, image_width))
+        self.set_boundary(boundary_points, boundary_type, self.__border)
         self.__label: Label | None = None
         self.set_label(label_id, label_name)
-        self.__border: Border | None = None
-        self.set_border(0, image_width, 0, image_height)
 
         self.id = new_id()
         self.valid = True
@@ -140,10 +59,6 @@ class Annotation:
     @property
     def label(self):
         return self.__label
-
-    @property
-    def border(self):
-        return self.__border
 
     @property
     def center(self):
@@ -162,40 +77,16 @@ class Annotation:
         if label_name is not None:
             self.__label.name = label_name
 
-    def set_boundary(self, points: np.ndarray, boundary_type: str, img_dims: tuple) -> None:
+    def set_boundary(self, points: np.ndarray, boundary_type: str, img_border: ImageBorder) -> None:
         if self.__boundary is None:
             for obj in BOUNDARY_TYPE_OBJS:
                 if obj.__name__ == boundary_type + BOUNDARY_NAME:
-                    self.__boundary = obj(points, img_dims)
+                    self.__boundary = obj(points, img_border)
                     break
             else:
                 raise ValueError(f"Boundary type '{boundary_type}' could not be found.")
         else:
             self.__boundary.set(points)
-
-    def set_border(
-            self,
-            x_min: Optional[int] = None,
-            x_max: Optional[int] = None,
-            y_min: Optional[int] = None,
-            y_max: Optional[int] = None
-    ) -> None:
-        """
-        Sets a new border within the coordinate system of the old border. Clips boundary to new border.
-        Args:
-            x_min (Optional - int): new min x value of border
-            x_max (Optional - int): new max x value of border
-            y_min (Optional - int): new min y value of border
-            y_max (Optional - int): new max y value of border
-        """
-        if self.__border is None:
-            self.__border = Border(x_max, y_max)
-            return
-        self.__border.set(x_min, x_max, y_min, y_max)
-        self.__border.rebase()
-        # adapt boundary
-        self.__boundary.shift(-x_min, -y_min)
-        self.__boundary.clip(0, self.__border.width, 0, self.__border.height)
 
     def verify(self) -> None:
         """
@@ -215,7 +106,7 @@ class Annotation:
         """
         Clips boundary to border
         """
-        # self.__boundary.clip(0, self.__border.width, 0, self.__border.height)
+        self.__boundary.clip()
 
 
 class Annotations:
@@ -230,6 +121,7 @@ class Annotations:
         self.width = image_width
         self.height = image_height
         self.boundary_type = boundary_type
+        self.border = ImageBorder(self.width, self.height)
 
     def __getitem__(self, index: int) -> Annotation:
         return self.annots[index]
@@ -241,11 +133,11 @@ class Annotations:
         """
         self.annots = [annot for annot in self.annots if not annot.valid]
 
-    def border(
+    def set_border(
             self,
             x_min: Optional[int] = None,
-            x_max: Optional[int] = None,
             y_min: Optional[int] = None,
+            x_max: Optional[int] = None,
             y_max: Optional[int] = None
     ) -> None:
         """
@@ -253,12 +145,11 @@ class Annotations:
         Reinitializes width and height.
         Args:
             x_min (Optional - int): new min x value of border
-            x_max (Optional - int): new max x value of border
             y_min (Optional - int): new min y value of border
+            x_max (Optional - int): new max x value of border
             y_max (Optional - int): new max y value of border
         """
-        for annot in self.annots:
-            annot.set_border(x_min, x_max, y_min, y_max)
+        self.border.set(x_min, y_min, x_max, y_max)
         if x_min is not None:
             self.width -= x_min
         if x_max is not None:
@@ -267,6 +158,22 @@ class Annotations:
             self.height -= y_min
         if y_max is not None:
             self.height -= self.height - y_max
+
+    def scale_border(self, x_scale: float = 1, y_scale: float = 1):
+        """
+        Scales the current border by an x and y factor
+        """
+        self.set_border(
+            0,
+            0,
+            int(x_scale * self.border.width),
+            int(y_scale * self.border.height)
+        )
+        self.width = self.width * x_scale
+        self.height = self.height * y_scale
+
+    def rebase_border(self):
+        self.border.rebase()
 
     def add(self, label_id: int, boundary_points: np.ndarray, label_name: Optional[str] = None) -> None:
         """
@@ -279,8 +186,7 @@ class Annotations:
         self.annots.append(Annotation(
             label_id,
             boundary_points,
-            self.width,
-            self.height,
+            self.border,
             self.boundary_type,
             label_name
         ))
@@ -316,12 +222,6 @@ class Annotations:
         """
         for annot in self.annots:
             annot.boundary.scale(x_scale, y_scale)
-            annot.set_border(
-                0,
-                int(x_scale * annot.border.width),
-                0,
-                int(y_scale * annot.border.height)
-            )
             annot.clip()
 
     def rotate(self, angle: float):

core/augmentations.py
@@ -1,15 +1,14 @@
+import math
 from typing import Optional
 
 import numpy as np
 import scipy as sp
 
-from .annotations import Annotations
 from .transforms import (
     SITransform,
     MITransform,
     IOTransform
 )
-from ..utils.mat_utils import get_2d_transf_mat
 
 
 class Shift(SITransform):
@@ -43,6 +42,7 @@ class Scale(SITransform):
         self.image = sp.ndimage.zoom(self.image, (self.x_scale, self.y_scale, 1))
 
     def _apply_on_annots(self):
+        self.annots.scale_border(self.x_scale, self.y_scale)
         self.annots.scale(self.x_scale, self.y_scale)
 
 
@@ -61,16 +61,143 @@ class Rotate(SITransform):
         self.annots.rotate(self.angle)
 
 
+class Resize(SITransform):
+    def __init__(self, target_width: int, target_height: int, preserve_aspect_ratio=True):
+        super().__init__()
+        self.target_width = target_width
+        self.target_height = target_height
+        self.preserve_aspect_ratio = preserve_aspect_ratio
+        self.img_width = None
+        self.img_height = None
+        self.extend = 0
 
-# def scale():
-#     pass
-#
-#
-# def rotate():
-#     pass
+    def _apply_on_image(self):
+        """
+        Here may be room for improvements... Its working - but maybe not as efficient as possible
+        """
+        self.img_width, self.img_height, _ = np.shape(self.image)
+        if not self.preserve_aspect_ratio:
+            self.image = sp.ndimage.zoom(
+                self.image,
+                (self.target_width / self.img_width, self.target_height / self.img_height, 1)
+            )
+        else:
+            asp_ratio = self.img_width / self.img_height
+            resized_asp_ratio = self.target_width / self.target_height
+            self.extend = math.ceil((asp_ratio - resized_asp_ratio) * self.img_width)
+            if self.extend > 0:
+                stack_a = np.zeros(
+                    (self.img_width, int(self.extend / 2), 3),
+                    dtype=np.uint8
+                )
+                stack_b = np.zeros(
+                    (self.img_width, self.extend - int(self.extend / 2), 3),
+                    dtype=np.uint8
+                )
+                self.image = np.hstack((stack_a, self.image, stack_b))
+            elif self.extend < 0:
+                stack_a = np.zeros(
+                    (int(-self.extend / 2), self.img_height, 3),
+                    dtype=np.uint8
+                )
+                stack_b = np.zeros(
+                    (-self.extend - int(-self.extend / 2), self.img_height, 3),
+                    dtype=np.uint8
+                )
+                self.image = np.vstack((stack_a, self.image, stack_b))
+            self.annots.set_border(x_max=np.shape(self.image)[0], y_max=np.shape(self.image)[1])
+            self.annots.rebase_border()
+            self.image = sp.ndimage.zoom(
+                self.image,
+                (self.target_width / np.shape(self.image)[0], self.target_height / np.shape(self.image)[1], 1)
+            )
+
+    def _apply_on_annots(self):
+        if self.preserve_aspect_ratio:
+            if self.extend > 0:
+                self.annots.scale_border(
+                    self.target_width / self.img_width,
+                    self.target_height / (self.img_height + self.extend)
+                )
+                self.annots.scale(
+                    self.target_width / self.img_width,
+                    self.target_height / (self.img_height + self.extend)
+                )
+                self.annots.shift(
+                    y_shift=int(self.extend * self.target_height / (2 * (self.img_height + self.extend)))
+                )
+
+            elif self.extend < 0:
+                self.annots.scale(
+                    self.target_width / (self.img_width - self.extend),
+                    self.target_height / self.img_height
+                )
+                self.annots.shift(
+                    x_shift=int(-self.extend * self.target_width / (2 * (self.img_width - self.extend)))
+                )
+        else:
+            self.annots.scale(self.target_width / self.img_width, self.target_height / self.img_height)
+
+
+class Mosaic(MITransform):
+    def __init__(self, mode: str = "resize"):
+        """
+        Stitches 4 images together to get one single image.
+        Args:
+            mode (str): 'resize' or 'crop' - Defines how the images are preprocessed. Images can either be resized or
+                        cropped to fit a unify size. The unify size is determined by the dimensions of the smallest
+                        image.
+        """
+        super().__init__()
+        self.mode = mode
+        self.unify_width = None
+        self.unify_height = None
+
+    def _preprocess(self):
+        preprocessed_images, preprocessed_annots = [], []
+        img_areas = [annots.border.area for annots in self.annots_list]
+        self.unify_width, self.unify_height = self.annots_list[img_areas.index(min(img_areas))].border.corners[1]
+        resizer = Resize(self.unify_width, self.unify_height)
+        # cropper = Crop()
+        for image, annots in zip(self.image_list, self.annots_list):
+            if annots.width == self.unify_width and annots.height == self.unify_height:
+                prep_img, prep_annots = image, annots
+                continue
+            if self.mode == "resize":
+                prep_img, prep_annots = resizer.apply(image, annots)
+            elif self.mode == "crop":
+                raise "NOT IMPLEMENTED YET"
+            else:
+                raise f"Unknown mode for Mosaic '{self.mode}'."
+            preprocessed_images.append(prep_img)
+            preprocessed_annots.append(prep_annots)
+        self.image_list = preprocessed_images
+        self.annots_list = preprocessed_annots
+
+    def _apply_on_images(self) -> None:
+        self.image = np.vstack((
+            np.hstack((self.image_list[0], self.image_list[3])),
+            np.hstack((self.image_list[1], self.image_list[2])),
+        ))
+
+    def _apply_on_annots(self) -> None:
+        for annots in self.annots_list:
+            annots.scale_border(2, 2)
+            annots.rebase_border()
+        self.annots_list[1].shift(y_shift=self.unify_height)
+        self.annots_list[2].shift(x_shift=self.unify_width, y_shift=self.unify_height)
+        self.annots_list[3].shift(x_shift=self.unify_width)
+        for idx, annots in enumerate(self.annots_list):
+            if idx == 0:
+                self.annots = annots
+            else:
+                for annot in annots:
+                    self.annots.add(annot.label.id, annot.boundary.points, annot.label.name)
+
 #
 #
 # def transform():
+#     perspective transform
 #     pass
 #
 #

core/boundaries.py
@@ -1,15 +1,23 @@
 import math
 from typing import List, Optional, Union
 
+from .borders import ImageBorder
+
 import numpy as np
 
 
 class Boundary:
-    def __init__(self, points: np.ndarray, img_dims: tuple):
+    def __init__(self, points: np.ndarray, img_border: ImageBorder):
         self._points = points
-        self.img_dims = img_dims
-        # self.validate()
+        self.border = img_border
+        self.validate()
         self.valid = True
+
+    def clean(self):
+        """
+        Cleans Boundary by removing duplicates.
+        """
+        self._points = np.unique(self._points, axis=0)
 
     def validate(self):
         """
@@ -52,8 +60,8 @@ class Boundary:
         return self._points
 
     @property
-    def center(self):
-        return self.__get_center()
+    def boundary_center(self):
+        return self.__get_boundary_center()
 
     @property
     def area(self):
@@ -63,7 +71,7 @@ class Boundary:
     def visualize(self):
         return self._points
 
-    def __get_center(self):
+    def __get_boundary_center(self):
         """
         Calculates the center of the boundary by the mid-point between min and max x and y.
         """
@@ -72,6 +80,17 @@ class Boundary:
             [
                 (np.min(points_x) + np.max(points_x)) / 2,
                 (np.min(points_y) + np.max(points_y)) / 2
+            ]
+        )
+
+    def __get_image_center(self):
+        """
+        Calculates the center of the image by the mid-point between min and max x and y.
+        """
+        return np.array(
+            [
+                self.border.width / 2,
+                self.border.height / 2
             ]
         )
 
@@ -88,28 +107,16 @@ class Boundary:
         if validate:
             self.validate()
 
-    def clip(
-            self,
-            x_min: Optional[int] = None,
-            x_max: Optional[int] = None,
-            y_min: Optional[int] = None,
-            y_max: Optional[int] = None
-    ) -> None:
+    def clip(self) -> None:
         """
-        Clips all points of boundary to new min and max x and y values.
-        Invalids boundary if all points of x or y are equalized.
-        Args:
-            x_min (Optional - int): new min x value of boundary points
-            x_max (Optional - int): new max x value of boundary points
-            y_min (Optional - int): new min y value of boundary points
-            y_max (Optional - int): new max y value of boundary points
+        Clips all points of boundary to image border. Sets new points of boundary.
         """
         points_x, points_y = self._points.T
         self.set(
             np.vstack(
                 (
-                    np.clip(points_x, x_min, x_max),
-                    np.clip(points_y, y_min, y_max)
+                    np.clip(points_x, self.border.x_min, self.border.x_max),
+                    np.clip(points_y, self.border.y_min, self.border.y_max)
                 )
             ).T
         )
@@ -144,6 +151,7 @@ class Boundary:
         Args:
             angle (float): Angle of rotation in deg
         """
+        img_center = self.__get_image_center()
         rad_angle = np.deg2rad(-angle)
         matrix = np.array(
             [
@@ -151,20 +159,8 @@ class Boundary:
                 [np.sin(rad_angle), np.cos(rad_angle)]
             ]
         )
-        # ox, oy = self.center
-        # adj_points_list = []
-        # for point in self._points:
-        #     x, y = point
-        #     qx = ox + math.cos(rad_angle) * (x - ox) + math.sin(rad_angle) * (y - oy)
-        #     qy = oy + -math.sin(rad_angle) * (x - ox) + math.cos(rad_angle) * (y - oy)
-        #     adj_points_list.append([qx, qy])
-        # points = np.array(adj_points_list)
-
-        adj_points = self._points - self.center
-        points = np.einsum("bi, ij -> bj", adj_points, matrix) + self.center
-        #points = np.vstack([np.matmul(point, matrix) for point in adj_points]) + self.center
-
-        # points = np.einsum("bi, ij -> bi", self.__points, matrix)
+        adj_points = self._points - img_center
+        points = np.einsum("bi, ij -> bj", adj_points, matrix) + img_center
         self.set(points)
 
 
@@ -172,9 +168,9 @@ class BBoxBoundary(Boundary):
     """
     Boundary for bounding boxes
     """
-    def __init__(self, points: np.ndarray, img_dims: tuple):
+    def __init__(self, points: np.ndarray, img_border: ImageBorder):
         self.__min_max_points = None
-        super().__init__(points, img_dims)
+        super().__init__(points, img_border)
 
     @property
     def points(self) -> np.ndarray:
@@ -229,8 +225,8 @@ class KeyPBoundary(Boundary):
     """
     Boundary for keypoints.
     """
-    def __init__(self, points: np.ndarray, img_dims: tuple):
-        super().__init__(points, img_dims)
+    def __init__(self, points: np.ndarray, img_border: ImageBorder):
+        super().__init__(points, img_border)
 
     def __get_area(self) -> float:
         """
@@ -243,5 +239,7 @@ class PolyBoundary(Boundary):
     """
     Boundary for polygons.
     """
-    def __init__(self, points: np.ndarray, img_dims: tuple):
-        super().__init__(points, img_dims)
+    def __init__(self, points: np.ndarray, img_border: ImageBorder):
+        super().__init__(points, img_border)
+
+

core/transforms.py
@@ -1,21 +1,22 @@
 from typing import List, Tuple, Optional
+from abc import ABC, abstractmethod
 
 import numpy as np
 
 from .annotations import Annotations
 
 
-class SITransform:
+class SITransform(ABC):
     """
     Single Image Transform
     """
-    def __init__(self, image: Optional[np.ndarray] = None, annots: Optional[Annotations] = None):
-        self.image = image
-        self.annots = annots
+    def __init__(self):
+        self.image = None
+        self.annots = None
 
     def apply(
             self,
-            image: Optional[np.ndarray] = None,
+            image: np.ndarray,
             annots: Optional[Annotations] = None
     ) -> Tuple[np.ndarray, Annotations]:
         """
@@ -28,16 +29,16 @@ class SITransform:
         Returns:
             (Tuple[np.ndarray, Annotations]): Tuple of transformed image and transformed annotations
         """
-        if image is not None:
-            self.image = image
-        if annots is not None:
-            self.annots = annots
+        self.image = image
+        self.annots = annots
         if self.image is None:
             raise ValueError("Unable to perform transformation. Image was not provided.")
         self._apply_on_image()
-        self._apply_on_annots()
+        if self.annots is not None:
+            self._apply_on_annots()
         return self.image, self.annots
 
+    @abstractmethod
     def _apply_on_image(self):
         """
         -- This method must be overwritten in a subclass --
@@ -46,6 +47,7 @@ class SITransform:
         """
         pass
 
+    @abstractmethod
     def _apply_on_annots(self):
         """
         -- This method must be overwritten in a subclass --
@@ -55,24 +57,42 @@ class SITransform:
         pass
 
 
-class MITransform:
+class MITransform(ABC):
     """
     Multi Image Transform
     """
-    def __init__(self, image_list: List[np.ndarray], annots_list: List[Annotations]):
-        self.image_list = image_list
-        self.annots_list = annots_list
-        self.shape_list = [np.shape(image)[:2] for image in self.image_list]
+    def __init__(self):
+        self.image_list = []
+        self.annots_list = []
         self.image = None
         self.annots = None
 
-    def combine_images(self):
+    def apply(
+            self,
+            image_list: List[np.ndarray],
+            annots_list: Optional[List[Annotations]] = None
+    ) -> Tuple[np.ndarray, Annotations]:
+        """
+        Applies the transformation to the image and its annotations.
+        Args:
+            image_list (List[np.ndarray]): Any slist of images as numpy array.
+            annots_list (Optional[List[Annotations]]): Annotations of images.
+        Returns:
+            (Tuple[np.ndarray, Annotations]): Tuple of transformed image and transformed annotations
+        """
+        self.image_list = image_list
+        self.annots_list = annots_list
+        self._apply_on_images()
+        if self.annots_list is not None:
+            self._apply_on_annots()
+        return self.image, self.annots
+
+    @abstractmethod
+    def _apply_on_images(self) -> None:
         pass
 
-    def apply_on_image(self) -> None:
-        pass
-
-    def apply_on_annots(self) -> None:
+    @abstractmethod
+    def _apply_on_annots(self) -> None:
         pass
 
 
@@ -80,8 +100,8 @@ class IOTransform:
     """
     Image Only Transform
     """
-    def __init__(self, image: np.ndarray):
-        self.image = image
+    def __init__(self):
+        pass
 
     def apply_on_image(self):
         pass

core/visualize.py
@@ -58,7 +58,7 @@ class Visualize:
     def _assemble(self):
         for annot in self.annots:
             # denormalized boundary
-            boundary = annot.boundary.points.astype(np.int32)
+            boundary = annot.boundary.visualize.astype(np.int32)
             boundary = boundary.reshape((-1, 1, 2))
             color = DEFAULT_COLOR if self.colors is None else self.colors[annot.label.name]
             self.image = cv2.polylines(